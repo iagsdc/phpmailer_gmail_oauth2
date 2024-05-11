91b9d154b0635c9554bc8f07fb555abef995a560
Fred Loehr <support@iagsdc.org>
2024-05-11 08:46:06 -0700

    adjustments to input form

--- src/Form/GmailOauth2SettingsForm.php
+++ src/Form/GmailOauth2SettingsForm.php
@@ -43,7 +43,7 @@ class GmailOauth2SettingsForm extends ConfigFormBase {
         $config->save();
 
         // get saved config data for display
-        $email_address = $config->get('gm_email_address');
+        $email_address = $this->configFactory->get('system.site')->get('mail');
         $client_id     = $config->get('gm_client_id');
         $client_secret = $config->get('gm_client_secret');
         $refresh_token = $config->get('gm_refresh_access_token');
@@ -66,23 +66,26 @@ class GmailOauth2SettingsForm extends ConfigFormBase {
             '#type'          => 'markup',
             '#markup'        => $this->t('<p>To retrieve a Refresh Token, fill out the form below and press the <strong>Save configuration</strong>. '              .
                                          'button. A <strong>Get Refresh Token</strong> button will appear after the configuration data has been saved.</p><p>  '    .
-                                         'Press the <strong>Get Refresh Token</strong> button to be redirected to '                                                 .
-                                         'Google, where you will sign in and follow the instructions. Assuming everything goes smoothly, you should eventually '    .
-                                         'be redirected back to this page, where the <strong>Refresh Token</strong> will display in a message at the top of the  '  .
-                                         'page.</p><p>There is no need to remember the token; it is only displayed to let you know it was received. Once you have ' .
-                                         'the Token, your setup is complete and email should work. Complete the setup on the Mail System and PHPMailer SMTP pages'),
+                                         'Press the <strong>Get Refresh Token</strong> button to be redirected to Google, where you will sign in and '              .
+                                         'follow the instructions. Assuming everything goes smoothly, you should be redirected back to this page, where '           .
+                                         'the <strong>Refresh Token</strong> will display in a message at the top of the page.</p><p>Once you have received '       .
+                                         'a Token, your setup is complete and email should work.</p><p>Be sure to complete the setup on the Mail System and '       .
+                                         'PHPMailer SMTP pages'),
         ];
 
     
         $form['gm_auth']['info3'] = [
             '#type'          => 'markup',
-            '#markup'        => $this->t('<p>The current Refresh Token is :token', [':token' => $refresh_token]),
+            '#markup'        => ((!$refresh_token || $refresh_token === 'Empty') ? 
+                                $this->t('<p><strong>Status:</strong> No Refresh Token Received, setup incomplete</p>') : $this->t('<p><strong>Status:</strong> Refresh Token Received and Stored, setup complete</p>')),
         ];
 
         $form['gm_auth']['gm_email_address'] = [
             '#type'          => 'textfield',
             '#title'         => $this->t('Email address'),
             '#default_value' => $email_address,
+            '#disabled'      => True,
+            '#description'   => $this->t('Email address is set on \'Basic site settings\' page'),
         ];
 
         $form['gm_auth']['gm_client_id'] = [
@@ -92,11 +95,11 @@ class GmailOauth2SettingsForm extends ConfigFormBase {
         ];
 
         $form['gm_auth']['gm_client_secret'] = [
-        #   '#type'          => 'textfield',
-            '#type'          => 'password',
+            '#type'          => 'textfield',
+        #   '#type'          => 'password',
             '#title'         => $this->t('Client secret'),
             '#default_value' => $client_secret,
-            '#description'   => $this->t('Leave empty to use the current secret'),
+        #   '#description'   => $this->t('Leave empty to use the current secret'),
         ];
 
         if($email_address && $client_id && $client_secret) {
@@ -105,10 +108,21 @@ class GmailOauth2SettingsForm extends ConfigFormBase {
                 '#type'          => 'link',
                 '#url'           => Url::fromRoute('phpmailer_gmail_oauth2.gmail_login'),
                 '#attributes'    => [
-                  'class' => ['button'],
+                    'class' => ['button'],
                 ],
             ];
         }
+
+#           '#disabled'      => (!$email_address || !$client_id || !$client_secret), 
+#       if(!$email_address || !$client_id || !$client_secret) {
+            $form['gm_auth']['info4'] = [
+                '#type'       => 'container',
+                '#access'     => (!$email_address || !$client_id || !$client_secret),
+                'content'     => [
+                    '#markup' => $this->t('<strong>Fill in Client Id and Client Secret and save to activate the \'Get Refresh Token\' button</strong>'),
+                ],
+            ];
+#       }
 
         return parent::buildForm($form, $form_state);
     }
@@ -122,13 +136,14 @@ class GmailOauth2SettingsForm extends ConfigFormBase {
 
         // Save the configuration changes.
         $config = $this->configFactory()->getEditable('phpmailer_gmail_oauth2.gmail_settings');
-        $config->set('gm_email_address', $values['gm_email_address']);
-        $config->set('gm_client_id', $values['gm_client_id']);
+    #   $config->set('gm_email_address', $values['gm_email_address']);
+        $config->set('gm_client_id',     $values['gm_client_id']);
+        $config->set('gm_client_secret', $values['gm_client_secret']);
 
         // Check if client secret is empty and don't overwrite current one if so.
-        if ($values['gm_client_secret'] !== '') {
-            $config->set('gm_client_secret', $values['gm_client_secret']);
-        }
+    #   if ($values['gm_client_secret'] !== '') {
+    #       $config->set('gm_client_secret', $values['gm_client_secret']);
+    #   }
 
         $config->save();
 
